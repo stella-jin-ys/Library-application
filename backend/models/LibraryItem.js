9ce85aeaf55c095b490d07090e2133f1aac057c0
Stella <stella.jin123@gmail.com>
2022-10-13 09:38:51 +0200

    update library item

--- backend/models/LibraryItem.js
+++ backend/models/LibraryItem.js
@@ -2,22 +2,30 @@ const mongoose = require("mongoose");
 
 const ItemSchema = new mongoose.Schema(
   {
-    book: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Book",
-    },
-    dvd: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "DVD",
-    },
-    audioBook: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "AudioBook",
-    },
-    reference: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Reference",
-    },
+    books: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+      },
+    ],
+    dvds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "DVD",
+      },
+    ],
+    audioBooks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "AudioBook",
+      },
+    ],
+    references: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Reference",
+      },
+    ],
     categoryId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Category",
